e8c11a790b123fcdddc2be3bd18e41b9a3e9783b
achraf faleh <faleh.ach24@triana.salesianos.edu>
2026-05-11 21:52:41 +0200

    feat(modelo): añadir entidades Repartidor, Pedido, Entrega, EntregaPedido y EstadoPedido

---
+++ src/main/java/com/salesianostriana/FoodDelivery_Manager/model/Entrega.java
@@ -0,0 +1,46 @@
+package com.salesianostriana.FoodDelivery_Manager.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Entrega {
+
+     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "La dirección es obligatoria")
+    private String direccion;
+
+    @NotNull(message = "La fecha es obligatoria")
+    private LocalDateTime fecha;
+
+    @Positive(message = "El tiempo debe ser positivo")
+    private Integer tiempo; // en minutos
+
+    @ManyToOne
+    @JoinColumn(name = "repartidor_id")
+    private Repartidor repartidor;
+
+    @OneToMany(mappedBy = "entrega", cascade = CascadeType.ALL)
+    private List<EntregaPedido> entregaPedidos;
+}
