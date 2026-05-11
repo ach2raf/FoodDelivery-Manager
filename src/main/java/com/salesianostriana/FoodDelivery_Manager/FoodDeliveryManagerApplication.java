9947c1c50f0cce7e89cff5ed205489cd7e289939
achraf faleh <faleh.ach24@triana.salesianos.edu>
2026-05-11 18:16:48 +0200

    Initial commit

---
+++ src/main/java/com/salesianostriana/FoodDelivery_Manager/FoodDeliveryManagerApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.FoodDelivery_Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveryManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salesianostriana/FoodDelivery_Manager/FoodDeliveryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.FoodDelivery_Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
