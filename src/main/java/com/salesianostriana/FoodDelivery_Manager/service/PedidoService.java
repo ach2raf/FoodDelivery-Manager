b35b1519305af31ec64d25ea11f675c694320752
achraf faleh <faleh.ach24@triana.salesianos.edu>
2026-05-18 23:24:37 +0200

    feat(pedido): añadir PedidoServico obtener todos

--- src/main/java/com/salesianostriana/FoodDelivery_Manager/service/PedidoService.java
+++ src/main/java/com/salesianostriana/FoodDelivery_Manager/service/PedidoService.java
@@ -1,5 +1,7 @@
 package com.salesianostriana.FoodDelivery_Manager.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.salesianostriana.FoodDelivery_Manager.model.Pedido;
@@ -21,6 +23,10 @@ public class PedidoService {
      public Pedido findById(Long id){
         return  pedidoRepository.findById(id)
         .orElseThrow(()->new RuntimeException("Pedido no encontrado con id: "+id));
+    }
+
+    public List<Pedido> findAll(){
+        return pedidoRepository.findAll();
     }
 
 
