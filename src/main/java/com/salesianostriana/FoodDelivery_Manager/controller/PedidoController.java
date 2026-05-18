df42cb25f89e5d6c57d429d7cac59d5dfa4a43a7
achraf faleh <faleh.ach24@triana.salesianos.edu>
2026-05-18 23:14:55 +0200

    feat(pedido): añadir controlador editar

--- src/main/java/com/salesianostriana/FoodDelivery_Manager/controller/PedidoController.java
+++ src/main/java/com/salesianostriana/FoodDelivery_Manager/controller/PedidoController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -41,6 +42,14 @@ public class PedidoController {
         return "redirect:/pedidos";
     }
 
+
+     @GetMapping("/editarPedido/{id}")
+    public String editar(@PathVariable Long id, Model model){
+
+
+        model.addAttribute("pedido", pedidoService.findById(id));
+        return "pedidoFormulario";
+    }
 
 
 }
