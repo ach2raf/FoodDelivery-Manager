e5f1aa72772cbe7c7b9f82436f78d4e7cd50555b
achraf faleh <faleh.ach24@triana.salesianos.edu>
2026-05-18 22:30:32 +0200

    feat(pedido): añadir controlador :1/mostrar formulario nuevo 2/guardar pedido

---
+++ src/main/java/com/salesianostriana/FoodDelivery_Manager/controller/PedidoController.java
@@ -0,0 +1,46 @@
+package com.salesianostriana.FoodDelivery_Manager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.salesianostriana.FoodDelivery_Manager.model.Pedido;
+import com.salesianostriana.FoodDelivery_Manager.service.PedidoService;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+
+@Controller
+@RequestMapping("/pedidos")
+@AllArgsConstructor
+
+public class PedidoController {
+
+        private final PedidoService pedidoService;
+
+     @GetMapping("/nuevoPedido")
+    public String nuevo(Model model){
+
+
+        model.addAttribute("pedido", new Pedido());
+        return "pedidoFormulario";
+    }
+
+     @PostMapping("/guardarPedido")
+    public String guardar(@Valid @ModelAttribute Pedido pedido, BindingResult result){
+
+
+        if(result.hasErrors()){
+            return "pedidoFormulario";
+        }
+        pedidoService.save(pedido);
+        return "redirect:/pedidos";
+    }
+
+
+
+}
