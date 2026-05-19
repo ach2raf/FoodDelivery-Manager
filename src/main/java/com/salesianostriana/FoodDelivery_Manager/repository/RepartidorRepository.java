1b66943a46f23603fc6acf89315d81e9bfb7b2d4
achraf faleh <faleh.ach24@triana.salesianos.edu>
2026-05-19 22:43:42 +0200

    feat(repartidor): añadir repositorio

---
+++ src/main/java/com/salesianostriana/FoodDelivery_Manager/repository/RepartidorRepository.java
@@ -0,0 +1,10 @@
+package com.salesianostriana.FoodDelivery_Manager.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.salesianostriana.FoodDelivery_Manager.model.Repartidor;
+@Repository
+public interface RepartidorRepository extends JpaRepository<Repartidor, Long>{
+
+}
