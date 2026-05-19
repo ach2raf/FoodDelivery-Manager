18c68c5984cc0f83d9843e75917b58eac38b72a8
achraf faleh <faleh.ach24@triana.salesianos.edu>
2026-05-19 23:18:48 +0200

    feat(repartidor): extender baseServiceImpl

--- src/main/java/com/salesianostriana/FoodDelivery_Manager/service/RepartidorService.java
+++ src/main/java/com/salesianostriana/FoodDelivery_Manager/service/RepartidorService.java
@@ -4,18 +4,25 @@ import org.springframework.stereotype.Service;
 
 import com.salesianostriana.FoodDelivery_Manager.model.Repartidor;
 import com.salesianostriana.FoodDelivery_Manager.repository.RepartidorRepository;
+import com.salesianostriana.FoodDelivery_Manager.service.base.BaseServiceImpl;
 
 import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
 
-public class RepartidorService {
+public class RepartidorService extends BaseServiceImpl<Repartidor, Long, RepartidorRepository>{
 
         private final RepartidorRepository repartidorRepository;
 
          public Repartidor save(Repartidor repartidor) {
         return repartidorRepository.save(repartidor);
     }
+
+    /*  public Repartidor findById(Long id) {
+        return repartidorRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Repartidor no encontrado con id: " + id));
+    }*/
+
 
 }
