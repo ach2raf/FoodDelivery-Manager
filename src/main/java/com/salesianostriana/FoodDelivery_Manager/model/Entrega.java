720d90b3264ca60e96a3f49addb377fe76bbfdf1
achraf faleh <faleh.ach24@triana.salesianos.edu>
2026-05-25 23:07:28 +0200

    feat(entrega): añadir entidad entrega

---
+++ src/main/java/com/salesianostriana/FoodDelivery_Manager/model/Entrega.java
@@ -0,0 +1,43 @@
+package com.salesianostriana.FoodDelivery_Manager.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Entrega {
+
+     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "La dirección es obligatoria")
+    private String direccion;
+
+    @NotNull(message = "La fecha es obligatoria")
+    private LocalDateTime fecha;
+
+    @Positive(message = "El tiempo debe ser positivo")
+    private Integer tiempo;
+
+    @ManyToOne
+    @JoinColumn(name = "repartidor_id")
+    private Repartidor repartidor;
+
+   
+}
