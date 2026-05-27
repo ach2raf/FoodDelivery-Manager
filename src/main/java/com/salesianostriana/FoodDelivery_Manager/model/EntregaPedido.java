eab42111fbfeadcff42acb0366f96d64f5000b09
achraf faleh <faleh.ach24@triana.salesianos.edu>
2026-05-27 22:05:11 +0200

    feat(entrega): añadir entidad EntregaPedido

---
+++ src/main/java/com/salesianostriana/FoodDelivery_Manager/model/EntregaPedido.java
@@ -0,0 +1,39 @@
+package com.salesianostriana.FoodDelivery_Manager.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EntregaPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private EstadoPedido estado = EstadoPedido.PENDIENTE;
+
+    private Double coste;
+
+    private Integer prioridad;
+
+    @ManyToOne
+    @JoinColumn(name = "entrega_id")
+    private Entrega entrega;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id")
+    private Pedido pedido;
+}
