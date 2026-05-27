336bf26e8a574c21a48bea8f076473013fee6e4e
achraf faleh <faleh.ach24@triana.salesianos.edu>
2026-05-27 22:54:56 +0200

    fix: hacer que PedidoService extienda de BaseServiceImpl

--- src/main/java/com/salesianostriana/FoodDelivery_Manager/service/PedidoService.java
+++ src/main/java/com/salesianostriana/FoodDelivery_Manager/service/PedidoService.java
@@ -1,34 +1,39 @@
 package com.salesianostriana.FoodDelivery_Manager.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.salesianostriana.FoodDelivery_Manager.model.Pedido;
 import com.salesianostriana.FoodDelivery_Manager.repository.PedidoRepository;
+import com.salesianostriana.FoodDelivery_Manager.service.base.BaseServiceImpl;
 
 import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
 
-public class PedidoService {
+public class PedidoService extends BaseServiceImpl<Pedido, Long, PedidoRepository>{
 
     private final PedidoRepository pedidoRepository;
 
+    @Override
      public Pedido save(Pedido pedido){
         return pedidoRepository.save(pedido);
     }
 
-     public Pedido findById(Long id){
-        return  pedidoRepository.findById(id)
-        .orElseThrow(()->new RuntimeException("Pedido no encontrado con id: "+id));
+    @Override
+     public Optional<Pedido> findById(Long id) {
+        return pedidoRepository.findById(id);
     }
 
+    @Override
     public List<Pedido> findAll(){
         return pedidoRepository.findAll();
     }
 
+    @Override
      public void deleteById(Long id){
         pedidoRepository.deleteById(id);
     }
