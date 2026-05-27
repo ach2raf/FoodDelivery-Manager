8893071e94459e0a7a13d790448445fd206b8b48
achraf faleh <faleh.ach24@triana.salesianos.edu>
2026-05-27 21:44:52 +0200

    feat(repartidor): añadir la relacion OneToMany entre repartidor e entrega

--- src/main/java/com/salesianostriana/FoodDelivery_Manager/model/Repartidor.java
+++ src/main/java/com/salesianostriana/FoodDelivery_Manager/model/Repartidor.java
@@ -1,9 +1,12 @@
 package com.salesianostriana.FoodDelivery_Manager.model;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,5 +28,8 @@ public class Repartidor {
     private String nombre;
     @NotBlank(message="la zona es obligatoria")
     private String zona;
+
+    @OneToMany(mappedBy = "repartidor")
+    private List<Entrega> entregas;
 
 }
