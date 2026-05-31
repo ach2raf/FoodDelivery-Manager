857e202a23d7513510e12ccadad479afe737c5fe
achraf faleh <faleh.ach24@triana.salesianos.edu>
2026-05-31 14:04:05 +0200

    feat(logica): añadir excepción RepartidorNoDisponibleException

---
+++ src/main/java/com/salesianostriana/FoodDelivery_Manager/exception/RepartidorNoDisponibleException.java
@@ -0,0 +1,7 @@
+package com.salesianostriana.FoodDelivery_Manager.exception;
+
+public class RepartidorNoDisponibleException extends RuntimeException {
+    public RepartidorNoDisponibleException(String mensaje) {
+        super(mensaje);
+    }
+}
